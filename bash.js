fd24f26924c7e8d19aaf9e1ae4e0ffeb0b1cd72f
Daniel Rodriguez <drodrigo95@hotmail.com>
2023-04-21 15:28:36 -0500

    ls.js

--- bash.js
+++ bash.js
@@ -1,3 +1,12 @@
+const pwd = require('./pwd.js')
+const fs = require('fs');
+const ls = require('./ls')
+
+console.log(ls);
+console.log(fs);
+console.log(pwd);
+pwd()
+
 
 const test = function(a,b) {
     console.log(a+b)
--- pwd.js
+++ pwd.js
@@ -1,10 +1,14 @@
-//Output a prompt
-process.stdout.write('prompt > ');
+module.exports = function() {
+  //Output a prompt
+  process.stdout.write('prompt > ');
 
-// The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim(); //remove the newline
+  // The stdin 'data' event fires after a user types in a line
+  process.stdin.on('data', (data) => {
+   const cmd = data.toString().trim(); //remove the newline
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-});
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  });
+
+  console.log('hello');
+}
